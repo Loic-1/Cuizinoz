961f6ddcfe5e7bc08c46b7daa803261c195f0863
Loic-1 <loic.vasile130@gmail.com>
2024-12-12 22:38:43 +0100

    Change logout to redirect to 'app_login'. Add UserController to try and display user's friends

---
+++ src/Controller/UserController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManager;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class UserController extends AbstractController
+{
+    #[Route('/user', name: 'app_user')]
+    public function index(UserRepository $userRepository): Response
+    {
+        $users = $userRepository->findAll();
+
+        return $this->render('user/index.html.twig', [
+            'users' => $users
+        ]);
+    }
+
+    #[Route('/{user}/friends', name: 'friends_user')]
+    public function listFriends(User $user): Response
+    {
+        $friends = $user->getFriends();
+
+        // var_dump($user, $friends); die;
+
+        return $this->render('user/listFriends.html.twig', [
+            'friends' => $friends
+        ]);
+    }
+}
