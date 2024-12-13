0dba0f5439238339367b4b9e386e9259d9cdce87
Loic-1 <loic.vasile130@gmail.com>
2024-12-13 17:01:39 +0100

    Can now add comments

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Recipe;
+use App\Form\CommentType;
 use App\Form\RecipeType;
 use App\Repository\RecipeRepository;
 use App\Repository\UserRepository;
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -45,10 +48,31 @@ class RecipeController extends AbstractController
     }
 
     #[Route('/recipe/{recipe}', name: 'detail_recipe')]
-    public function detailRecipe(Recipe $recipe): Response
+    public function detailRecipe(Recipe $recipe, Request $request, EntityManagerInterface $entityManager): Response
     {
+        $comment = new Comment();
+
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            
+            $comment = $form->getData();
+            $comment->setCreationDate(new DateTime());
+            $comment->setUser($this->getUser());
+            $comment->setRecipe($recipe);
+
+            $entityManager->persist($comment);
+            $entityManager->flush();
+
+            return $this->redirectToRoute("detail_recipe", [
+                "recipe" => $recipe->getId()
+            ]);
+        }
+
         return $this->render('recipe/detailRecipe.html.twig', [
-            'recipe' => $recipe
+            'recipe' => $recipe,
+            'addCommentForm' => $form
         ]);
     }
 }
