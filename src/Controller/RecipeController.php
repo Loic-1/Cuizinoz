6dbb8942a52affa16d582b32b897ae4ecc370bf1
Loic-1 <loic.vasile130@gmail.com>
2024-12-16 09:39:40 +0100

    Add edit recipe view to edit recipe attributes

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -55,8 +55,8 @@ class RecipeController extends AbstractController
         $form = $this->createForm(CommentType::class, $comment);
         $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid()) {
-            
+        if ($form->isSubmitted() && $form->isValid()) {
+
             $comment = $form->getData();
             $comment->setCreationDate(new DateTime());
             $comment->setUser($this->getUser());
@@ -73,6 +73,34 @@ class RecipeController extends AbstractController
         return $this->render('recipe/detailRecipe.html.twig', [
             'recipe' => $recipe,
             'addCommentForm' => $form
+        ]);
+    }
+
+    #[Route('/recipe/edit/{recipe}', name: 'edit_recipe')]
+    public function editRecipe(Recipe $recipe, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        // le 2ème paramètre permet de "préremplir" le form avec les données de $recipe
+        $form = $this->createForm(RecipeType::class, $recipe);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // on récupère les entrées dans les champs du form
+            $recipe = $form->getData();
+
+            // on prépare le push vers la BDD
+            $entityManager->persist($recipe);
+            // on push les données d'un coup, rapide et efficace -> https://www.doctrine-project.org/projects/doctrine-orm/en/3.3/reference/working-with-objects.html#persisting-entities
+            $entityManager->flush();
+
+            return $this->redirectToRoute("detail_recipe", [
+                "recipe" => $recipe->getId()
+            ]);
+        }
+
+        return $this->render('recipe/editRecipe.html.twig', [
+            'recipe' => $recipe,
+            'editRecipeForm' => $form
         ]);
     }
 }
