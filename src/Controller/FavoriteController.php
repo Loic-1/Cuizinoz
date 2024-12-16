3b51b0092e9370446be4847cf0f755f322accdb0
Loic-1 <loic.vasile130@gmail.com>
2024-12-16 11:28:03 +0100

    Add addFavorites function to add recipe to user's favorites list

--- src/Controller/FavoriteController.php
+++ src/Controller/FavoriteController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Favorite;
 use App\Entity\User;
+use App\Entity\Recipe;
 use App\Repository\FavoriteRepository;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,6 +21,22 @@ class FavoriteController extends AbstractController
 
         return $this->render('favorite/index.html.twig', [
             'favorites' => $favorites
+        ]);
+    }
+    #[Route('favorite/add/{recipe}/{user}', name: 'add_favorite')]
+    public function addFavorite(Recipe $recipe, User $user, EntityManagerInterface $entityManager)
+    {
+        $favorite = new Favorite();
+
+        $favorite->setUser($user);
+        $favorite->setRecipe($recipe);
+        $favorite->setRegisterDate(new DateTime());
+
+        $entityManager->persist($favorite);
+        $entityManager->flush();
+
+        return $this->redirectToRoute("app_favorite", [
+            "user" => $user->getId()
         ]);
     }
 }
