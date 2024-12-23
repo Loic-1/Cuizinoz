5b455c06a6f437a51c7c7fb929bee0c2899f676b
Loic-1 <loic.vasile130@gmail.com>
2024-12-23 17:02:04 +0100

    Define necessary queries to DB for homepage to work properly

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,6 +11,12 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        // recettes à la une (trié en fonction de la note et des commentaires, définir un max(par défaut: 6))
+
+        // derniers commentaires (définir un max(par défaut: 6))
+
+        // 
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
