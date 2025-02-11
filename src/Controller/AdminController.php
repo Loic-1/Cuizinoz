74b684af80eedee452df0a22adf17ce0ec6327b2
Loic-1 <loic.vasile130@gmail.com>
2025-02-11 12:58:32 +0100

    UPDATE: Add $users in AdminController::index return

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class AdminController extends AbstractController
 {
-    #[Route('/admin', name: 'app_admin')]
-    public function index(): Response
+    #[Route('/admin/users', name: 'app_admin')]
+    public function index(UserRepository $userRepository): Response
     {
+        $users = $userRepository->findAll();
+
         return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+            'users' => $users,
         ]);
     }
 }
