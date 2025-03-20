023383fc4b16f696eb8ca046ebd008803ba6b944
Loic-1 <loic.vasile130@gmail.com>
2025-03-20 08:42:10 +0100

    UPDATE: Change input type of comment text from text to textarea

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -7,6 +7,7 @@ use App\Entity\Recipe;
 use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,7 +18,8 @@ class CommentType extends AbstractType
     {
         $builder
             ->add('title', TextType::class)
-            ->add('content', TextType::class)
+            // ->add('content', TextType::class)
+            ->add('content', TextareaType::class)
         ;
     }
 
