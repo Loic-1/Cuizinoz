ec1fea7f7f2482d590f1cb69f97cc7ac47b849b7
Loic-1 <loic.vasile130@gmail.com>
2025-03-24 15:52:51 +0100

    UPDATE (minor): Add label to inputs in CommentType

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -3,9 +3,6 @@
 namespace App\Form;
 
 use App\Entity\Comment;
-use App\Entity\Recipe;
-use App\Entity\User;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -17,9 +14,12 @@ class CommentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title', TextType::class)
-            // ->add('content', TextType::class)
-            ->add('content', TextareaType::class)
+            ->add('title', TextType::class, [
+                'label' => 'Titre'
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Contenu'
+            ])
         ;
     }
 
