0f355e64301853016a647218343986645af5d18e
Loic-1 <loic.vasile130@gmail.com>
2025-04-01 09:16:29 +0200

    ADD: Style for form

--- src/Form/RecipeType.php
+++ src/Form/RecipeType.php
@@ -11,6 +11,8 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class RecipeType extends AbstractType
@@ -18,15 +20,24 @@ class RecipeType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class)
-            ->add('preparationTime')
-            ->add('instructions', TextType::class)
+            ->add('name', TextType::class, [
+                'label' => 'Nom recette'
+            ])
+            ->add('preparationTime', NumberType::class, [
+                'label' => 'Temps de préparation'
+            ])
+            ->add('instructions', TextareaType::class, [
+                'label' => 'Instructions de préparation'
+            ])
             ->add('category', EntityType::class, [
                 'class' => Category::class,
                 'choice_label' => 'name',
+                'expanded' => true,
+                'label' => 'Catégorie plat',
             ])
             ->add('images', FileType::class, [
                 'label' => false,
+                // 'label' => 'Illustrations',
                 'multiple' => true,
                 'mapped' => false,
                 'required' => false
